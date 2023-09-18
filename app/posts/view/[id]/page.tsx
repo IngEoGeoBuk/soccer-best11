e4f9bb128bde102124467a6dd674c7d3c7f09260
vaultmicroSeongwoo <you3667@vaultmicro.com>
2023-09-18 18:31:40 +0900

    fix: server url 수정 및 relation 수정

--- app/posts/view/[id]/page.tsx
+++ app/posts/view/[id]/page.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import View from './view';
 
 export const generateMetadata = async ({ params }: any): Promise<Metadata> => {
-  const { data } = await axios.get(`http://localhost:3000/api/posts/${params.id}`);
+  const { data } = await axios.get(`${process.env.SOCCER_BEST11_SERVER as string}/api/posts/${params.id}`);
   return {
     title: `${data?.title} - Soccer Best11`,
     description: data?.description,
