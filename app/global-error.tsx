a78c8825e190ec38f525cc40f9473d48da2f6bd1
vaultmicroSeongwoo <you3667@vaultmicro.com>
2024-03-20 16:49:12 +0900

    chore: Can not find name's quick fix 시 @/app/_actions이 아닌 @actions 뜨게 수정

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -14,6 +14,7 @@ export default function Error({
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
+    // eslint-disable-next-line no-console
     console.error(error);
   }, [error]);
 
