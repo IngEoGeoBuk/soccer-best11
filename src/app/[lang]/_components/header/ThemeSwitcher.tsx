71721f550ebe0eec8f25de4263993beba6a7a4f1
IngEoGeoBuk <you3667@gmail.com>
2024-05-03 21:15:24 +0900

    fix: 헤더 다크모드 스위치 아이콘 하나만 보이게 수정

--- src/app/[lang]/_components/header/ThemeSwitcher.tsx
+++ src/app/[lang]/_components/header/ThemeSwitcher.tsx
@@ -19,7 +19,7 @@ function ThemeSwitcher() {
     >
       <svg
         id="theme-toggle-dark-icon"
-        className="w-5 h-5 mr-[-10px] mt-[5px]"
+        className="w-5 h-5 hidden dark:block"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
@@ -30,7 +30,7 @@ function ThemeSwitcher() {
       </svg>
       <svg
         id="theme-toggle-light-icon"
-        className="w-5 h-5 mb-[5px]"
+        className="w-5 h-5 dark:hidden"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
